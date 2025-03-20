3e74c749e5348c9e58e18c3a5b766a4920edd151
Olaofe Opeyemi <emmanuelolaofe5@gmail.com>
2025-03-20 13:57:09 +0100

    changed the theme

--- start_model.py
+++ start_model.py
@@ -4,4 +4,4 @@ import torch
 # Load GPT-2 tokenizer and model
 MODEL_NAME = "gpt3_turbo"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+ 
